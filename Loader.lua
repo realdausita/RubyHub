c9ac077a6db1be71fa963689d7a285ad30df7e8b
realdausita <yagizdelikan3560@gmail.com>
2026-06-14 21:45:56 +0300

    Add support for loading scripts with caching

--- Loader.lua
+++ Loader.lua
@@ -5,27 +5,31 @@ if not game:IsLoaded() then game.Loaded:Wait() end
 local pId = game.PlaceId
 
 if pId == 4620170611 or pId == 3851622790 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/BreakIn.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/BreakIn.lua", true))()
 elseif pId == 9872472334 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Evade.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Evade.lua", true))()
 elseif pId == 18687417158 or pId == 83645629621104 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Forsaken.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Forsaken.lua", true))()
 elseif pId == 2753915549 or pId == 4442272183 or pId == 7449423635 or pId == 100117331123089 or pId == 79091703265657 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Bloxfruit.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Bloxfruit.lua", true))()
 elseif pId == 142823291 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/mm2.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/mm2.lua", true))()
 elseif pId == 13864661000 or pId == 13864667823 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/BreakIn2.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/BreakIn2.lua", true))()
 elseif pId == 70876832253163 or pId == 116495829188952 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/DeadRails.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/DeadRails.lua", true))()
 elseif pId == 79268393072444 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Sell%20Lemons.lua", true))()
 elseif pId == 72988931267790 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/DrawASleigh%26SlideDownHill.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/DrawASleigh%26SlideDownHill.lua", true))()
+elseif pId == 97598239454123 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/growagarden2", true))()
 elseif pId == 95082159892680 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/%2B1%20Speed%20Escape%20on%20Keyboard%20Candy%20and%20Chocalete.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/%2B1%20Speed%20Escape%20on%20Keyboard%20Candy%20and%20Chocalete.lua", true))()
 elseif pId == 920587237 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/AdoptMe.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/AdoptMe.lua", true))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Universall.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realdausita/RubyHub/refs/heads/main/Universall.lua", true))()
 end
+
+print("Ruby Hub Loaded!")
